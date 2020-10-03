b228f2457f1c50f5d6ccf9f2f5215b859ed10aa7
Hanlin Cheng <hanlin.cheng@uwaterloo.ca>
2020-10-03 18:35:45 -0400

    added example of styling with simple button and colour change

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,18 +1,35 @@
 import * as React from 'react';
+import styled from "styled-components";
 
 interface Props {
    name:
     string
 }
 
+const Title = styled.h1`
+  font-size: 3em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Button = styled.button`
+  display: flex;    
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  
+`;
+
 class App extends React.Component<Props> {
   render() {
     const { name } = this.props;
     return (
       <>
-        <h1>
-          Hello {name}
-        </h1>
+          <Title>Hello {name}</Title>
+          <Button>Click Me!</Button>
       </>
     );
   }
