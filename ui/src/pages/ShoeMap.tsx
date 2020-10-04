0d9a8ae7fab9e03733e5e37c419863f32dda735a
Leslie Xin <leslie.xin@shopify.com>
2020-10-04 14:03:02 -0400

    Adding in react router and ShoeMap page

---
+++ ui/src/pages/ShoeMap.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export const ShoeMap = ()=> {
+    return (
+        <div>
+          <h1>ShoeMap</h1> 
+          <span> test text fkjbkjbjdbvdjfv</span> 
+        </div>
+    );
+  }
+  
+  
