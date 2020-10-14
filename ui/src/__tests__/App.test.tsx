628da87c1b7b625e2adadd1dcab46c1599f01202
Leslie Xin <leslie.xin@shopify.com>
2020-10-14 19:46:29 -0400

    adding frontend testing

---
+++ ui/src/__tests__/App.test.tsx
@@ -0,0 +1,5 @@
+describe("<App />", () => {
+  test("test", async () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
--- ui/src/styles/colors.js
+++ ui/src/styles/colors.js
@@ -1,4 +1,3 @@
-const white = "#FFFFFF";
 const black = "#000000";
 const pink = "#E9A9A9";
 
