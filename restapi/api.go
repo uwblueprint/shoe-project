84406bfacc67c2a72dc19e847245d1d616f415df
Shehryar Suleman <shehryarsuleman@gmail.com>
2020-10-14 21:53:04 -0400

    add create-author endpoint

--- restapi/api.go
+++ restapi/api.go
@@ -1,9 +1,12 @@
 package restapi
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/render"
+	"github.com/uwblueprint/shoe-project/internal/database/models"
 	"github.com/uwblueprint/shoe-project/restapi/rest"
 	"github.com/uwblueprint/shoe-project/server"
 	"go.uber.org/zap"
@@ -30,6 +33,27 @@ func Router(db *gorm.DB) (http.Handler, error) {
 	rest.PostHandler(r, "/authors", api.CreateAuthors)
 	rest.PostHandler(r, "/stories", api.CreateStories)
 	return r, nil
+}
+
+func (api api) authorCreate(w http.ResponseWriter, r *http.Request) render.Renderer {
+	// Declare a new Story struct.
+	var s []models.Author
+
+	// Try to decode the request body into the struct. If there is an error,
+	// respond to the client with the error message and a 400 status code.
+	err := json.NewDecoder(r.Body).Decode(&s)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return rest.MsgStatusOK("Error")
+	}
+
+	if err := api.database.Create(&s).Error; err != nil {
+		return rest.ErrInternal(zap.S(), err)
+	}
+
+	// Do something with the Author struct...
+	fmt.Fprintf(w, "Author: %+v", s)
+	return rest.MsgStatusOK("Hello World")
 }
 
 func (api api) health(w http.ResponseWriter, r *http.Request) render.Renderer {
