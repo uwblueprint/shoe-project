7d67fdb18c578f3a40ce68a3cdd6daba982b0c09
Megan Penny <pennymegan1357@gmail.com>
2020-10-14 22:01:05 -0400

    Get stories (#46)
    * get stories and get story by id endpoints
    
    * Refactor stories endpoints into a seperate file
    
    * PR fixes
    
    Co-authored-by: s278gupt <s278gupt@edu.uwaterloo.ca>

--- restapi/api.go
+++ restapi/api.go
@@ -6,21 +6,26 @@ import (
 	"github.com/go-chi/render"
 	"github.com/uwblueprint/shoe-project/restapi/rest"
 	"github.com/uwblueprint/shoe-project/server"
+	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
 // namespace api
 type api struct {
 	database *gorm.DB
+	logger   *zap.SugaredLogger
 }
 
 func Router(db *gorm.DB) (http.Handler, error) {
 	r := server.CreateRouter()
 	api := api{
 		database: db,
+		logger:   zap.S(),
 	}
 
 	rest.GetHandler(r, "/health", api.health)
+	rest.GetHandler(r, "/stories", api.ReturnAllStories)
+	rest.GetHandler(r, "/story/{storyID}", api.ReturnStoryByID)
 
 	return r, nil
 }
---
+++ restapi/stories.go
@@ -0,0 +1,38 @@
+package restapi
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
+	"github.com/uwblueprint/shoe-project/internal/database/models"
+	"github.com/uwblueprint/shoe-project/restapi/rest"
+	"gorm.io/gorm"
+)
+
+func (api api) ReturnAllStories(w http.ResponseWriter, r *http.Request) render.Renderer {
+	var stories []models.Story
+
+	err := api.database.Find(&stories).Error
+	if err != nil {
+		return rest.ErrInternal(api.logger, err)
+	}
+
+	return rest.JSONStatusOK(stories)
+}
+
+func (api api) ReturnStoryByID(w http.ResponseWriter, r *http.Request) render.Renderer {
+	var story models.Story
+	id := chi.URLParam(r, "storyID")
+
+	err := api.database.Where("id=?", id).First(&story).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return rest.ErrNotFound(fmt.Sprintf("Could not find story with ID %s", id))
+		}
+		return rest.ErrInternal(api.logger, err)
+	}
+
+	return rest.JSONStatusOK(story)
+}
