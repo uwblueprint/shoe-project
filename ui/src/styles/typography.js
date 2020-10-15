9b0c1285b9f138c874c7b7504314070d7f5b28a5
Hanlin Cheng <hanlin.cheng@uwaterloo.ca>
2020-10-14 20:13:27 -0400

    Adding Github Actions workflow  (#48)
    github actions workflow for node and go

--- ui/src/styles/typography.js
+++ ui/src/styles/typography.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 import colors from "./colors";
 import { device } from "./device";
 
-const extraLargeText = "42px";
+// const extraLargeText = "42px";
 const largeText = "36px";
-const mediumText = "16px";
+// const mediumText = "16px";
 const smallText = "14px";
-const extraSmallText = "12px";
-const tinyText = "10px";
+// const extraSmallText = "12px";
+// const tinyText = "10px";
 
 export const TitleText = styled.p`
   font-size: ${largeText};
