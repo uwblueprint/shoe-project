9fcd6aa7e2b8a99f205047369608b54e06342a05
Shehryar Suleman <shehryarsuleman@gmail.com>
2020-10-18 13:48:33 -0400

    made changes to CreateAuthor

---
+++ restapi/author.go
@@ -0,0 +1,26 @@
+package restapi
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/render"
+	"github.com/uwblueprint/shoe-project/internal/database/models"
+	"github.com/uwblueprint/shoe-project/restapi/rest"
+)
+
+func (api api) CreateAuthor(w http.ResponseWriter, r *http.Request) render.Renderer {
+	// Declare a new Story struct.
+	var author []models.Author
+
+	// respond to the client with the error message and a 400 status code.
+	if err := json.NewDecoder(r.Body).Decode(&author); err != nil {
+		rest.ErrInvalidRequest(api.logger, "Invalid payload", err)
+	}
+
+	if err := api.database.Create(&author).Error; err != nil {
+		return rest.ErrInternal(api.logger, err)
+	}
+
+	return rest.MsgStatusOK("Authors added successfully")
+}
