a804569ab1262fcdf49e93cd6b07cbde9aff5698
Shehryar Suleman <shehryarsuleman@gmail.com>
2020-10-18 14:54:50 -0400

    added return statement

--- restapi/authors.go
+++ restapi/authors.go
@@ -15,7 +15,7 @@ func (api api) CreateAuthors(w http.ResponseWriter, r *http.Request) render.Rend
 
 	// respond to the client with the error message and a 400 status code.
 	if err := json.NewDecoder(r.Body).Decode(&author); err != nil {
-		rest.ErrInvalidRequest(api.logger, "Invalid payload", err)
+		return rest.ErrInvalidRequest(api.logger, "Invalid payload", err)
 	}
 
 	if err := api.database.Create(&author).Error; err != nil {
--- restapi/stories.go
+++ restapi/stories.go
@@ -44,7 +44,7 @@ func (api api) CreateStories(w http.ResponseWriter, r *http.Request) render.Rend
 
 	// respond to the client with the error message and a 400 status code.
 	if err := json.NewDecoder(r.Body).Decode(&stories); err != nil {
-		rest.ErrInvalidRequest(api.logger, "Invalid payload", err)
+		return rest.ErrInvalidRequest(api.logger, "Invalid payload", err)
 	}
 
 	if err := api.database.Create(&stories).Error; err != nil {
