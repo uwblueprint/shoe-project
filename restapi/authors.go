5c88cbb92cee3dcff57df211b39bc1d928c400bc
Shehryar Suleman <shehryarsuleman@gmail.com>
2020-10-18 14:56:31 -0400

    add create-stories endpoint (#55)
    * Add create-stories endpoint
    
    * made changes to CreateStory
    
    * Changed function name
    
    * removed eslint file
    
    * added return statement

--- restapi/authors.go
+++ restapi/authors.go
@@ -9,13 +9,13 @@ import (
 	"github.com/uwblueprint/shoe-project/restapi/rest"
 )
 
-func (api api) CreateAuthor(w http.ResponseWriter, r *http.Request) render.Renderer {
-	// Declare a new Story struct.
+func (api api) CreateAuthors(w http.ResponseWriter, r *http.Request) render.Renderer {
+	// Declare a new Author struct.
 	var author []models.Author
 
 	// respond to the client with the error message and a 400 status code.
 	if err := json.NewDecoder(r.Body).Decode(&author); err != nil {
-		rest.ErrInvalidRequest(api.logger, "Invalid payload", err)
+		return rest.ErrInvalidRequest(api.logger, "Invalid payload", err)
 	}
 
 	if err := api.database.Create(&author).Error; err != nil {
