e0193c9742fa00caf73a82d2afd9769358566dfa
Dinu Wijetunga <50289930+DinuWije@users.noreply.github.com>
2020-10-18 15:11:55 -0400

    Make DB models JSON friendly (#67)

--- internal/database/models/models.go
+++ internal/database/models/models.go
@@ -6,18 +6,18 @@ import (
 
 type Author struct {
 	gorm.Model
-	FirstName     string `gorm:"not null"`
-	LastName      string
-	Bio           string  `gorm:"type:text"`
-	OriginCountry string  `gorm:"not null"`
-	CurrentCity   string  `gorm:"not null"`
-	Stories       []Story `gorm:"foreignKey:AuthorID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	FirstName     string  `gorm:"not null" json:"first_name"`
+	LastName      string  `json:"last_name"`
+	Bio           string  `gorm:"type:text" json:"bio"`
+	OriginCountry string  `gorm:"not null" json:"origin_country"`
+	CurrentCity   string  `gorm:"not null" json:"current_city"`
+	Stories       []Story `gorm:"foreignKey:AuthorID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"stories,omitempty"`
 }
 
 type Story struct {
 	gorm.Model
-	Title    string `gorm:"not null"`
-	Content  string `gorm:"type:text;not null"`
-	AuthorID uint
-	Author   Author
+	Title    string `gorm:"not null" json:"title"`
+	Content  string `gorm:"type:text;not null" json:"content"`
+	AuthorID uint   `json:"author_id"`
+	Author   Author `json:"author"`
 }
