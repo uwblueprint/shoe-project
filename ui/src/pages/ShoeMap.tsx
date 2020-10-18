9422708430c3b1fbb5357a34f29837e05ed1e4a6
Leslie Xin <leslie.xin@shopify.com>
2020-10-18 14:21:18 -0400

    added leaflet map and dependencies

--- ui/src/pages/ShoeMap.tsx
+++ ui/src/pages/ShoeMap.tsx
@@ -1,11 +1,35 @@
 import * as React from "react";
+import styled from "styled-components";
+import { Map, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 import { TitleText } from "../styles/typography";
 
+const StyledMap = styled(Map)`
+  height: 70vh;
+  width: 90vw;
+`;
+
+const MapContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export const ShoeMap: React.FC = () => {
+  const zoom = 12;
+  const currentLocation = { lat: 43.4723, lng: -80.5449 };
+
   return (
-    <div>
-      <TitleText>ShoeMap</TitleText>
-      <span> test text</span>
-    </div>
+    <MapContainer>
+      <TitleText>Shoe Map</TitleText>
+      <StyledMap center={currentLocation} zoom={zoom}>
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/{z}/{x}/{y}?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`}
+        />
+      </StyledMap>
+    </MapContainer>
   );
 };
