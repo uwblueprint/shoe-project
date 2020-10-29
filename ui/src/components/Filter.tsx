6268885158c3328e89afa645c6eee68bdf5f271d
dinu <dinu.wijetunga@gmail.com>
2020-10-28 23:28:08 -0400

    Pepared data loading for filter

--- ui/src/components/Filter.tsx
+++ ui/src/components/Filter.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
+import useSWR from "swr";
 import styled from "styled-components";
 import { colors } from "../styles/colors";
 
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import TextField from "@material-ui/core/TextField";
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 export interface FilterProps {
   options: string[];
   onChange: (options: string[]) => void;
 }
 
-const countries = ["China", "India", "Greece"];
+// const countries = ["China", "India", "Greece"];
 
 const FilterContainer = styled.div`
   position : absolute;
@@ -29,14 +31,20 @@ const FilterContainer = styled.div`
 `;
 
 export function Filter(): JSX.Element {
+  const { data:countries, error } = useSWR<string[]>("/api/authors/countries");
+
+  if (error) return <div>Error!</div>;
+  const loading = open; //&& options.length === 0;
+
   return (
     <FilterContainer>
       <span style={{fontSize: 16}}>Show stories from:</span>
       &nbsp; &nbsp;
       <Autocomplete
         multiple
+        loading={!countries}
         id="filter-autocomplete"
-        options={countries}
+        options={countries || []}
         style={{ width: 312}}
         renderInput={(params) => (
           <TextField
@@ -44,6 +52,15 @@ export function Filter(): JSX.Element {
             variant="standard"
             label=""
             placeholder="Countries"
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <React.Fragment>
+                  {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                  {params.InputProps.endAdornment}
+                </React.Fragment>
+              ),
+            }}
           />
         )}
       />
