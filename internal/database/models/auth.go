22b10a4daad37b288b030fe92599ec0fdfd7a74c
Megan Penny <pennymegan1357@gmail.com>
2020-11-04 21:34:56 -0500

    Fix linter errors (#86)
    * fix linter errors
    
    * PR fixes

--- internal/database/models/auth.go
+++ internal/database/models/auth.go
@@ -7,7 +7,7 @@ import (
 
 type User struct {
 	gorm.Model
-	Username string `json:"username";gorm:"unique"`
+	Username string `json:"username" gorm:"unique"`
 	Password string `json:"password"`
 }
 
