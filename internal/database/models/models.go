b74983809897c3f3ff376e0fbf568d94082d4a10
s278gupt <s278gupt@edu.uwaterloo.ca>
2020-11-06 00:22:06 +0300

    working composite primary key solution

--- internal/database/models/models.go
+++ internal/database/models/models.go
@@ -1,26 +1,33 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Author struct {
-	gorm.Model
-	FirstName     string  `gorm:"not null" json:"first_name"`
-	LastName      string  `json:"last_name"`
-	Bio           string  `gorm:"type:text" json:"bio"`
-	OriginCountry string  `gorm:"not null" json:"origin_country"`
-	Stories       []Story `gorm:"foreignKey:AuthorID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"stories,omitempty"`
+	ID            uint `gorm:"autoIncrement:true"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	DeletedAt     gorm.DeletedAt `gorm:"index"`
+	FirstName     string         `gorm:"primaryKey;not null" json:"first_name"`
+	LastName      string         `gorm:"primaryKey" json:"last_name"`
+	Bio           string         `gorm:"type:text" json:"bio"`
+	OriginCountry string         `gorm:"primaryKey;not null" json:"origin_country"`
+	Stories       []Story        `gorm:"foreignKey:AuthorFirstName,AuthorLastName,AuthorCountry;references:FirstName,LastName,OriginCountry;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"stories,omitempty"`
 }
 
 type Story struct {
 	gorm.Model
-	Title       string  `gorm:"not null" json:"title"`
-	Content     string  `gorm:"type:text;not null" json:"content"`
-	CurrentCity string  `gorm:"not null" json:"current_city"`
-	Summary     string  `gorm:"type:text" json:"summary"`
-	Latitude    float64 `gorm:"not null" json:"latitude"`
-	Longitude   float64 `gorm:"not null" json:"longitude"`
-	AuthorID    uint    `json:"author_id"`
-	Author      Author  `json:"author"`
+	Title           string  `gorm:"not null" json:"title"`
+	Content         string  `gorm:"type:text;not null" json:"content"`
+	CurrentCity     string  `gorm:"not null" json:"current_city"`
+	Summary         string  `gorm:"type:text" json:"summary"`
+	Latitude        float64 `gorm:"not null" json:"latitude"`
+	Longitude       float64 `gorm:"not null" json:"longitude"`
+	AuthorFirstName string  `json:"author_first_name"`
+	AuthorLastName  string  `json:"author_last_name"`
+	AuthorCountry   string  `json:"author_country"`
+	Author          Author  `gorm:"foreignKey:AuthorFirstName,AuthorLastName,AuthorCountry;references:FirstName,LastName,OriginCountry" json:"author"`
 }
