754ba81fb920fa80eafa893c1c49dffddd767777
Abhijeet Prasad <devabhiprasad@gmail.com>
2020-11-08 14:14:44 -0500

    chore: Update colors

--- ui/src/styles/colors.ts
+++ ui/src/styles/colors.ts
@@ -1,32 +1,35 @@
 /* Primary Colours */
 const teal = "#6FBAC6";
-const tealDark1 = "#2D8694";
-const tealDark2 = "#1D626D";
+const tealDark1 = "#153959";
+const tealDark2 = "#2D6394";
 const tealLight1 = "#8CC7D1";
 const tealLight2 = "#B7DFE5";
 const tealLight3 = "#D2EDF1";
 const tealLight4 = "#EBF7F9";
+const tealLight5 = "#EAF4F4";
 
 /* Secondary Colours */
 const red = "#C42626";
-const redDark1 = "#8F1717";
-const redDark2 = "#6F1010";
+const redDark1 = "#6F1010";
+const redDark2 = "#8F1717";
 const redLight1 = "#E44343";
 const redLight2 = "#EA7B7B";
 const redLight3 = "#F5B8B8";
 const redLight4 = "#FCDCDC";
 
 /* Neutrals */
-const neutral = "#EAF4F4";
-const grey = "#F2F6F8";
+const light = "#F2F6F8";
+const dark = "#1E202D";
+const grey = "#7C7C7C";
 const black = "#000000";
 const white = "#FFFFFF";
 
 export const colors = Object.freeze({
-  neutral,
-  grey,
   black,
   white,
+  neutralLight: light,
+  neutralDark: dark,
+  neutralGrey: grey,
   primary: teal,
   primaryDark1: tealDark1,
   primaryDark2: tealDark2,
@@ -34,6 +37,7 @@ export const colors = Object.freeze({
   primaryLight2: tealLight2,
   primaryLight3: tealLight3,
   primaryLight4: tealLight4,
+  primaryLight5: tealLight5,
   secondary: red,
   secondaryDark1: redDark1,
   secondaryDark2: redDark2,
