f61419f47e3ddc38f7c1363ca1c4369b841e2af9
Shehryar Suleman <shehryarsuleman@gmail.com>
2020-11-22 15:21:34 -0500

    PR fixes

--- internal/database/migrations/migrations.go
+++ internal/database/migrations/migrations.go
@@ -38,7 +38,7 @@ func Seed(db *gorm.DB) error {
 				Summary:     faker.Hacker().SaySomethingSmart(),
 				Latitude:    float64(faker.Address().Latitude()),
 				Longitude:   float64(faker.Address().Longitude()),
-				ImageURL:    "ExampleURL",
+				ImageURL:    "https://exampleurl.com",
 			},
 		},
 	}
