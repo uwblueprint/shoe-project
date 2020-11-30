990c509355703e8fae1a88c83838addaaaed4622
Hanlin Cheng <h62cheng@uwaterloo.ca>
2020-11-30 17:20:31 -0500

    finished component + styling

--- ui/src/components/WelcomeScreens.tsx
+++ ui/src/components/WelcomeScreens.tsx
@@ -8,45 +8,78 @@ import MuiDialogContent from "@material-ui/core/DialogContent";
 import MuiDialogActions from "@material-ui/core/DialogActions";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
-import ShoeLogo from "../assets/images/shoeproject-logo.svg";
+import ShoeLogo from "../assets/images/test.svg";
 
 import {
-  CardTitleText,
-  CardDescriptionText,
+  WelcomeTitleText,
+  WelcomeDescriptionText,
   CardDetailText,
   CardTagText,
 } from "../styles/typography";
 
-const StyledWelcome = styled(Dialog)`
-  width: 490px;
-  height: 346px;
-  left: 475px;
-  top: 277px;
+const StyledIconButton = styled(Button)`
+  position: absolute;
+  align-items: right;
+  margin-left: 455px;  
+  margin-top: 29px;
+ 
+  && {
+    align: right;
+    box-shadow: none;
+    width: 14px;
+    height: 14px;
+  }
+  }
+`;
 
-  background: #ffffff;
-  box-shadow: 0px 0px 25px 5px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
+const StyledWelcome = styled(Dialog)`
+  .MuiDialog-paper {
+    width: 490px;
+    height: 346px;
+    background: #ffffff;
+    box-shadow: 0px 0px 25px 5px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    margin: 0px;
+  }
 `;
 
 const StyledButton = styled(Button)`
-  && {
+    position: absolute;
+    left: 134px;
+    bottom: 24px;
+
+&& {
     box-shadow: none;
     background-color: ${colors.primaryLight4};
     width: 222px;
     height: 48px;
-
+    display: flex;
     &:active {
       background-color: ${colors.primaryLight3};
     }
   }
 `;
 
-const StyledLogo = styled.div`
-  background-image: url(${ShoeLogo});
-  width: 87px;
-  height: 87px;
+const StyledLogo = styled.img`
+  margin-left: 189px;
+  margin-right: 196px;
+  margin-top: 32px;
+  margin-bottom: 16px;
+  display: flex;
+  background-repeat: no-repeat;
+  width: 105px;
+  height: 109px;
 `;
 
+const StyledCloseIcon = styled(CloseIcon)`
+position: absolute;
+right: 21px;
+top: 29px;
+
+  .MuiIcon-colorPrimary {
+    color: #2d6394;
+  }
+`;
 export function WelcomeScreens() {
   const [open, setOpen] = React.useState(false);
 
@@ -57,21 +90,25 @@ export function WelcomeScreens() {
   return (
     <>
       <StyledWelcome open={true}>
-          <StyledLogo></StyledLogo>
-        <CardTitleText>Welcome</CardTitleText>
-        <CardDescriptionText>
+        {/* <StyledIconButton onClick={handleClose}> */}
+          <StyledCloseIcon color="primary" fontSize="small" />
+        {/* </StyledIconButton> */}
+        <StyledLogo src = {ShoeLogo}></StyledLogo>
+        <WelcomeTitleText>Welcome</WelcomeTitleText>
+        <WelcomeDescriptionText>
           The Shoe Project Impact Page is an interactive map that showcases a
           selection of stories written by immigrant and refugee women who have
           come to Canada.
-        </CardDescriptionText>
+        </WelcomeDescriptionText>
         <StyledButton
-          variant="contained"
+          text-color={colors.primaryDark2}
+          text-align="center"
+          variant="text"
           color={colors.primaryLight2}
           disableElevation={true}
         >
           START TOUR
         </StyledButton>
-        {/* <MuiDialogTitle>Testing</MuiDialogTitle> */}
       </StyledWelcome>
     </>
   );
--- ui/src/styles/typography.ts
+++ ui/src/styles/typography.ts
@@ -52,6 +52,29 @@ export const CardDescriptionText = styled.p`
   }
 `;
 
+export const WelcomeTitleText = styled.p`
+  font-size: ${fontSize.h3Text};
+  font-weight: 500;
+  line-height: 30px;
+  text-align: center;
+  margin: 0px;
+  color: ${colors.black};
+`;
+
+export const WelcomeDescriptionText = styled.p`
+  font-size: ${fontSize.body2};
+  font-weight: normal;
+  line-height: 21px;
+  text-align: center;
+  align-items: center;
+  color: ${colors.black};
+  opacity: 0.7;
+  padding-bottom: 24px;
+  padding-left: 16px;
+  padding-right: 16px;
+
+  `;
+
 export const CardDetailText = styled.p`
   font-size: ${fontSize.caption};
   font-weight: 300;
