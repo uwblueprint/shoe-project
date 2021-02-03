15247b6d20095e64dcd7382da1e510dcc11997c2
Hanlin Cheng <h62cheng@uwaterloo.ca>
2021-02-02 22:27:20 -0500

    initial commit for upload stories

---
+++ ui/src/pages/Upload.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import useSWR from "swr";
+
+
+export const Upload: React.FC = () => {
+  return (
+      <>
+    <div>
+        Hello World
+        </div>
+      </>
+  )
+};
