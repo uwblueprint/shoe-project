bc0ebea86affd2c03ada5a47c85df13cc4ce06ea
dinu <dinu.wijetunga@gmail.com>
2021-02-09 22:14:04 -0500

    feat: rough CSS and functionality for login page

--- ui/src/pages/Login.tsx
+++ ui/src/pages/Login.tsx
@@ -1,74 +1,68 @@
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 import * as React from "react";
 import { Redirect } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
+
 import { useAuth } from "../hooks/auth";
+import { LoginMessageText, LoginTitleText } from "../styles/typography";
+
+const CardDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  position: absolute;
+`;
 
 const StyledCard = styled(Card)`
-  align-self: center;
-  max-width: 10vw;
-  max-height: 30vh;
-  /* display: flex;
-  justify-content: center;
-  align-items: center; */
+  max-height: 35vh;
+  max-width: 25vw;
 `;
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-    maxWidth: 275,
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginTop: 10,
-  },
-});
 
-export const Login: React.FC = () => {
+const GoogleButton = styled.div`
+  text-align: center;
+  margin-bottom: 5vh;
+`;
+
+interface LoginProps {
+  login: boolean;
+}
+
+export const Login: React.FC<LoginProps> = (props) => {
   const auth = useAuth();
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+
+  const title = props.login
+    ? "Welcome to the Shoe Project Admin Portal"
+    : "Sorry, that is not a valid email";
+  const description = props.login
+    ? "Please Sign In using your Shoe Project Email"
+    : "Make sure you are using a Shoe Project Organization Email";
 
   if (auth.user) {
     return <Redirect to="/admin" />;
   }
 
   return (
-    <StyledCard className={classes.root} variant="outlined">
-      <CardContent>
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          Welcome to the Shoe Project Admin Portal
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          Hel
-          <br />
-          {'"HEHEHEH"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </StyledCard>
+    <CardDiv>
+      <StyledCard variant="outlined">
+        <CardContent>
+          <LoginTitleText>{title}</LoginTitleText>
+          <LoginMessageText>{description}</LoginMessageText>
+        </CardContent>
+        <GoogleButton>
+          <Button
+            size="small"
+            color="primary"
+            variant="contained"
+            onClick={auth.signin}
+          >
+            Sign In with Google
+          </Button>
+        </GoogleButton>
+      </StyledCard>
+    </CardDiv>
   );
 };
