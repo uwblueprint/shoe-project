ddf51a66bfcdfe82e07a105b0fd31cfb65fcb850
Shehryar Suleman <shehryarsuleman@gmail.com>
2021-02-13 13:58:41 -0500

    make endpoint public

--- restapi/api.go
+++ restapi/api.go
@@ -47,6 +47,7 @@ func Router(db *gorm.DB, locationFinder location.LocationFinder) (http.Handler, 
 		rest.GetHandler(r, "/story/{storyID}", api.ReturnStoryByID)
 		rest.GetHandler(r, "/authors/origin_countries", api.ReturnAllCountries)
 		rest.PostHandler(r, "/login", api.Login)
+		rest.PostHandler(r, "/story", api.CreateStoriesFormData)
 
 		rest.GetHandler(r, "/client_tokens", api.ReturnClientTokens)
 	})
@@ -55,7 +56,7 @@ func Router(db *gorm.DB, locationFinder location.LocationFinder) (http.Handler, 
 	r.Group(func(r chi.Router) {
 		r.Use(jwtauth.Verifier(config.GetJWTKey()))
 		r.Use(jwtauth.Authenticator)
-		rest.PostHandler(r, "/story", api.CreateStoriesFormData)
+
 		rest.PostHandler(r, "/stories", api.CreateStories)
 		rest.PostHandler(r, "/authors", api.CreateAuthors)
 	})
