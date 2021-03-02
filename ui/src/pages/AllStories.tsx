7007dbe3a35e2276990c44d1eb3a066fa6b91803
Hanlin Cheng <h62cheng@uwaterloo.ca>
2021-03-02 00:40:13 -0500

    fix: format and lint

--- ui/src/pages/AllStories.tsx
+++ ui/src/pages/AllStories.tsx
@@ -1,25 +1,28 @@
-import clsx from 'clsx';
-import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
-import TableCell from '@material-ui/core/TableCell';
-import Paper from '@material-ui/core/Paper';
-import { AutoSizer, Column, Table, TableCellRenderer, TableHeaderProps } from 'react-virtualized';
-import { colors } from "../styles/colors";
-import { device } from "../styles/device";
-import { Story } from "../types";
-import { Grid } from "@material-ui/core/";
+import Paper from "@material-ui/core/Paper";
+import {
+  createStyles,
+  Theme,
+  WithStyles,
+  withStyles,
+} from "@material-ui/core/styles";
+import Switch from "@material-ui/core/Switch";
+import TableCell from "@material-ui/core/TableCell";
+import clsx from "clsx";
 import * as React from "react";
-import styled from "styled-components";
+import {
+  AutoSizer,
+  Column,
+  Table,
+  TableCellRenderer,
+  TableHeaderProps,
+} from "react-virtualized";
 import useSWR from "swr";
 
-import Switch from '@material-ui/core/Switch';
+import { colors } from "../styles/colors";
+import { Story } from "../types";
 
-import { FixedSizeList as List } from "react-window";
-declare module '@material-ui/core/styles/withStyles' {
-  // Augment the BaseCSSProperties so that we can control jss-rtl
+declare module "@material-ui/core/styles/withStyles" {
   interface BaseCSSProperties {
-    /*
-     * Used to control if the rule-set should be affected by rtl transformation
-     */
     flip?: boolean;
   }
 }
@@ -27,30 +30,33 @@ declare module '@material-ui/core/styles/withStyles' {
 const styles = (theme: Theme) =>
   createStyles({
     flexContainer: {
-      display: 'flex',
-      alignItems: 'center',
-      boxSizing: 'border-box',
+      display: "flex",
+      alignItems: "left",
+      boxSizing: "border-box",
     },
     table: {
-      '& .ReactVirtualized__Table__headerRow': {
+      "& .ReactVirtualized__Table__headerRow": {
         flip: false,
-        paddingRight: theme.direction === 'rtl' ? '0 !important' : undefined,
+        paddingRight: theme.direction === "rtl" ? "0 !important" : undefined,
+        backgroundColor: colors.primaryLight4,
+        fontWeight: 800,
       },
+      "& .ReactVirtualized__Grid ReactVirtualized__Table__Grid": {},
     },
     tableRow: {
-      cursor: 'pointer',
+      cursor: "pointer",
       color: colors.grey,
     },
     tableRowHover: {
-      '&:hover': {
+      "&:hover": {
         backgroundColor: colors.black,
       },
     },
     tableCell: {
-      flex: 1,
+      flex: 2,
     },
     noClick: {
-      cursor: 'initial',
+      cursor: "initial",
     },
   });
 
@@ -71,14 +77,16 @@ interface MuiVirtualizedTableProps extends WithStyles<typeof styles> {
   headerHeight?: number;
   onRowClick?: () => void;
   rowCount: number;
-  rowGetter: (row: Row) => Stories;
+  rowGetter: (row: Row) => TableData;
   rowHeight?: number;
 }
 
-class MuiVirtualizedTable extends React.PureComponent<MuiVirtualizedTableProps> {
+class MuiVirtualizedTable extends React.PureComponent<
+  MuiVirtualizedTableProps
+> {
   static defaultProps = {
     headerHeight: 48,
-    rowHeight: 48,
+    rowHeight: 70,
   };
 
   getRowClassName = ({ index }: Row) => {
@@ -99,25 +107,37 @@ class MuiVirtualizedTable extends React.PureComponent<MuiVirtualizedTableProps> 
         })}
         variant="body"
         style={{ height: rowHeight }}
-        align={(columnIndex != null && columns[columnIndex].numeric) || false ? 'right' : 'left'}
+        align={
+          (columnIndex != null && columns[columnIndex].numeric) || false
+            ? "right"
+            : "left"
+        }
       >
-        {columns[columnIndex].toggle &&       <Switch disabled inputProps={{ 'aria-label': 'disabled checkbox' }} />
-}
-        {cellData.toString()}
+        {columns[columnIndex].toggle && (
+          <Switch disabled inputProps={{ "aria-label": "disabled checkbox" }} />
+        )}
+        {cellData}
       </TableCell>
     );
   };
 
-  headerRenderer = ({ label, columnIndex }: TableHeaderProps & { columnIndex: number }) => {
+  headerRenderer = ({
+    label,
+    columnIndex,
+  }: TableHeaderProps & { columnIndex: number }) => {
     const { headerHeight, columns, classes } = this.props;
 
     return (
       <TableCell
         component="div"
-        className={clsx(classes.tableCell, classes.flexContainer, classes.noClick)}
+        className={clsx(
+          classes.tableCell,
+          classes.flexContainer,
+          classes.noClick
+        )}
         variant="head"
         style={{ height: headerHeight }}
-        align={columns[columnIndex].numeric || false ? 'right' : 'left'}
+        align={columns[columnIndex].numeric || false ? "right" : "left"}
       >
         <span>{label}</span>
       </TableCell>
@@ -125,18 +145,24 @@ class MuiVirtualizedTable extends React.PureComponent<MuiVirtualizedTableProps> 
   };
 
   render() {
-    const { classes, columns, rowHeight, headerHeight, ...tableProps } = this.props;
+    const {
+      classes,
+      columns,
+      rowHeight,
+      headerHeight,
+      ...tableProps
+    } = this.props;
     return (
       <AutoSizer>
         {({ height, width }) => (
           <Table
             height={height}
             width={width}
-            rowHeight={rowHeight!}
+            rowHeight={rowHeight}
             gridStyle={{
-              direction: 'inherit',
+              direction: "inherit",
             }}
-            headerHeight={headerHeight!}
+            headerHeight={headerHeight}
             className={classes.table}
             {...tableProps}
             rowClassName={this.getRowClassName}
@@ -167,7 +193,7 @@ class MuiVirtualizedTable extends React.PureComponent<MuiVirtualizedTableProps> 
 
 const VirtualizedTable = withStyles(styles)(MuiVirtualizedTable);
 
-interface Stories {
+interface TableData {
   id: number;
   title: string;
   city: string;
@@ -177,7 +203,6 @@ interface Stories {
   is_visible: boolean;
 }
 
-
 function createData(
   id: number,
   title: string,
@@ -185,78 +210,79 @@ function createData(
   year: string,
   author: string,
   country: string,
-  is_visible: boolean,
-): Stories{
-  return { id, title, city, year, author, country , is_visible};
+  is_visible: boolean
+): TableData {
+  return { id, title, city, year, author, country, is_visible };
 }
 
 export const AllStories: React.FC = () => {
   const { data: allStories, error } = useSWR<Story[]>("/api/stories");
 
-
-  type Sample = [number, string, string, string, string, string, boolean];
-
-const sample: Sample[] = [
-  [0, "Test Story 1", "Toronto", "2021", "Hanlin Cheng", "China", true],
-];
-if (allStories) {
-  var rows: Stories[] = [];
-  for (let i = 0; i < 33; i += 1) {
-    const randomSelection = sample[Math.floor(Math.random() * sample.length)];
-    const randomSelection2 = [i, allStories[i].title, allStories[i].current_city, allStories[i].year, allStories[0].author_first_name, allStories[0].author_country, allStories[i].is_visible];
-    rows.push(createData(i, allStories[i].title, allStories[i].current_city, allStories[i].year, allStories[0].author_first_name, allStories[0].author_country, allStories[i].is_visible));
+  if (allStories) {
+    const rows: TableData[] = [];
+    for (let i = 0; i < 33; i += 1) {
+      rows.push(
+        createData(
+          i,
+          allStories[i].title,
+          allStories[i].current_city,
+          allStories[i].year,
+          allStories[i].author_first_name,
+          allStories[i].author_country,
+          allStories[i].is_visible
+        )
+      );
+    }
   }
-}
-
-
 
   if (error) return <div>Error returning stories data!</div>;
   if (!allStories) return <div>Loading all stories table..</div>;
+
   return (
-    <Paper style={{ height: 1500, width: '100%' }}>
+    <Paper style={{ height: 1500, width: "100%" }}>
       <VirtualizedTable
         rowCount={rows.length}
         rowGetter={({ index }) => rows[index]}
         columns={[
           {
             width: 50,
-            label: 'ID',
-            dataKey: 'id',
+            label: "ID",
+            dataKey: "id",
             numeric: true,
           },
           {
-            width: 200,
-            label: 'Story Name',
-            dataKey: 'title',
+            width: 300,
+            label: "Story Name",
+            dataKey: "title",
           },
           {
             width: 120,
-            label: 'Current City',
-            dataKey: 'city',
+            label: "Current City",
+            dataKey: "city",
           },
           {
             width: 100,
-            label: 'Year',
-            dataKey: 'year',
+            label: "Year",
+            dataKey: "year",
+          },
+          {
+            width: 300,
+            label: "Author Name",
+            dataKey: "author",
+          },
+          {
+            width: 250,
+            label: "Country of Origin",
+            dataKey: "country",
           },
           {
             width: 150,
-            label: 'Author Name',
-            dataKey: 'author',
-          },
-          {
-            width: 200,
-            label: 'Country of Origin',
-            dataKey: 'country',
-          },
-          {
-            width: 150,
-            label: 'Show on Map',
-            dataKey: 'is_visible',
+            label: "Show on Map",
+            dataKey: "is_visible",
             toggle: true,
           },
         ]}
       />
     </Paper>
   );
-}
+};
