adc0e79f0d93c335467ea68610adab03b3abea61
m-penny <pennymegan1357@gmail.com>
2021-03-06 14:28:30 -0500

    Fix map initialization

--- restapi/stories.go
+++ restapi/stories.go
@@ -60,7 +60,7 @@ func (api api) ReturnStoryByID(w http.ResponseWriter, r *http.Request) render.Re
 func (api api) CreateStories(w http.ResponseWriter, r *http.Request) render.Renderer {
 	// Declare a new Story struct.
 	var stories []models.Story
-	var cityToCount map[string]int64
+	cityToCount := make(map[string]int64)
 	// respond to the client with the error message and a 400 status code.
 	if err := json.NewDecoder(r.Body).Decode(&stories); err != nil {
 		return rest.ErrInvalidRequest(api.logger, "Invalid payload", err)
