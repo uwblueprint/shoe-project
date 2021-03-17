001c071b6dbb91bbf7799ef3c938d97848ecbb1f
Hanlin Cheng <h62cheng@uwaterloo.ca>
2021-03-17 00:34:21 -0400

    fix: empty render condition on sort

--- ui/src/pages/AllStories.tsx
+++ ui/src/pages/AllStories.tsx
@@ -61,6 +61,7 @@ export const AllStories: React.FC = () => {
   const [tabValue, setTabValue] = React.useState(0);
   const [visibleState, setVisibleState] = React.useState([]);
   const [tableData, setTableData] = useState(rows);
+
   const [selectedRowIds, setSelectedRowIds] = useState([]);
   const [order, setOrder] = useState("desc");
   const [orderBy, setOrderBy] = useState("id");
@@ -105,7 +106,11 @@ export const AllStories: React.FC = () => {
       if (order !== 0) return order;
       return a[1] - b[1];
     });
-    return stabilizedThis.map((el) => el[0]);
+    const sortedArray = stabilizedThis.map((el) => el[0]);
+    if (sortedArray.length === 0) {
+      return rows;
+    }
+    return sortedArray;
   };
   const getComparator = (order, orderBy) => {
     return order === "desc"
@@ -123,7 +128,6 @@ export const AllStories: React.FC = () => {
 
   const indeterminate =
     selectedRowIds.length > 0 && selectedRowIds.length !== rows.length;
-
   if (error) return <div>Error returning stories data!</div>;
   if (!allStories) return <div>Loading all stories table..</div>;
 
@@ -148,7 +152,7 @@ export const AllStories: React.FC = () => {
       </StyledAppBar>
       <AllStoriesTabs value={tabValue} index={0}>
         <VirtualizedTable
-          data={rows}
+          data={stableSort(tableData, getComparator(order, orderBy))}
           order={order}
           orderBy={orderBy}
           columns={[
