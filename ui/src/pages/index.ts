920ce91037d34d00e50fbf15a469d384b1112bd5
Abhijeet Prasad <devabhiprasad@gmail.com>
2021-04-03 14:03:25 -0400

    index

--- ui/src/pages/index.ts
+++ ui/src/pages/index.ts
@@ -2,4 +2,3 @@ export { Admin } from "./Admin";
 export { AllStories } from "./AllStories/index";
 export { Login } from "./Login";
 export { ShoeMap } from "./ShoeMap";
-export { Upload } from "./Upload";
